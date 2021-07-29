a60571304d8da27e55099a7aebc21a01c72c7fc8
Sergio Sepulveda <sergiosepulveda09@live.com>
2021-07-29 15:59:10 -0700

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Sergio Sepulveda on 2021-07-29.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
